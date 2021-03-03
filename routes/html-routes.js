89bafe8ba4fd40cc27fde21d35e9b3f1b6b506fa
swhachey <sw.hachey@gmail.com>
2021-03-03 12:07:39 -0700

    got first route working altered server.js to do that

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,13 +1,14 @@
 const path = require('path');
+var exphbs = require("express-handlebars");
 
 module.exports = (app) => {
   app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '../public/index.html'))
-  );
-  app.get('/post', (req, res) =>
-    res.sendFile(path.join(__dirname, '../public/post.html'))
-  );
-  app.get('/search', (req, res) =>
-    res.sendFile(path.join(__dirname, '../public/search.html'))
-  );
+    res.render("index"))
+  ;
+//   app.get('/post', (req, res) =>
+//     res.sendFile(path.join(__dirname, '../public/post.html'))
+//   );
+//   app.get('/search', (req, res) =>
+//     res.sendFile(path.join(__dirname, '../public/search.html'))
+//   );
 }
